f5e5f1aa70b82279c8a4048c6d02d1d6bde62af0
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-21 12:57:23 -0800

    mis a jour

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H_ 
 #define MAIN_H_
 
-#include"0-putchar.c"
-#include "1-alphabet.c"
-print_Putchar();
-print_alphabet();
+
+int print_Putchar(void);
+void print_alphabet(void);
 #endif
