a82c5ca4d6a7074dd8a17a5419a28282e9ae8004
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-21 12:33:46 -0800

    print all alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
-/**
-  * main - Entry program
-  *
-  * Return: always 0 (success)
-  */
 /*function declaration*/
 void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
 
 /**
  * print_alphabet - print all alphabet
