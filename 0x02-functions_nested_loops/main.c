4774be65590353ca27ef30921f401a2a873edd1a
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-21 06:08:54 -0800

    print _putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - Entry program
+  *
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+        
+
+	print_Putchar();
+
+    	return (0);
+}
+
