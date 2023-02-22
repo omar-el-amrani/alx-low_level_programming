690a72b06aae221afe58902588be7ecde9f36026
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-21 18:58:56 -0800

    if it alphabet return 1 else return 0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*function declaration*/
+int _isalpha(int c);
+
+
+/**
+ * _isalphar - check if char alphabet
+ * @c: This is longer description of C
+ * Return: return 1 if it alphabet and 0 if anything else
+ */
+int _isalpha(int c)
+{
+	if (65 <= c && 90 >= c && 97 <= c && 122 >= c)
+	{
+		return (1);
+	}
+	return (0);
+}
