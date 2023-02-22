40c066a8a5557522cc158e9e4c562a54e1ec20df
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-21 19:44:41 -0800

    if greater then 0 or less or zero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,12 +11,12 @@ int print_sign(int n);
  */
 int print_sign(int n)
 {
-	if ( 0 < n )
+	if (n < 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if ( 0 > n )
+	else if (n > 0)
 	{
 		_putchar('-');
 		return (-1);
