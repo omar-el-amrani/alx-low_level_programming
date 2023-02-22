582716fe697912ff44cde77cbe599a0b2a31c174
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-21 18:24:50 -0800

    return 1 if it lowercase and 0 if not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*function declaration*/
+int _islower(int c);
+
+
+/**
+ * _islower - check if char lowercase or uppercase
+ * Return: return 1 if it lowercase and 0 if it uppercase
+ */
+int _islower(int c)
+{
+	if(96<c && c>123)
+	{
+		return 1;
+	}
+	else
+	{
+                return 0;
+        }
+}
