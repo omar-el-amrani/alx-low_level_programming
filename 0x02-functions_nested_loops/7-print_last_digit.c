3f5cdca4c1115c9b50e85bcd5f8c4020f1b83889
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-22 15:11:53 -0800

    get last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/*function declaration*/
+int print_last_digit(int);
+
+
+/**
+ * print_last_digit - get last digit
+ * @n: This is longer description of n
+ * Return: return last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
