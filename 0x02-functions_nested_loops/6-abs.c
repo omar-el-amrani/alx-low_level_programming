43de776d8c7a4c129170dd81678936306a968831
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-22 11:37:24 -0800

    absolute function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/*function declaration*/
+int _abs(int);
+
+
+/**
+ * _abs - absolute value 
+ * @n: This is longer description of n
+ * Return: return absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-1 * n);
+	}
+	return (0);
+}
