fc554a1a14a3420aad70260ea87bbd641cdb7056
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-22 16:25:08 -0800

    print hours and minutes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /*function declaration*/
-void jack_bauer(void);
+void times_table(void);
 
 
 /**
- * jack_bauer - print hours and minutes
+ * times_table - print 9 time table
  * Return: return void
  */
-void jack_bauer(void)
+void times_table(void)
 {
-	int i = 0;
-	int j = 0;
+	int i=0;
+	int j=0;
 	while (23 >= i)
 	{
 		while (59 >= j)
 		{
-			printf("%02d:%02d\n", i, j);
+			printf("%02d:%02d\n",i,j);
 			j++;
 		}
-		j = 0;
+		j=0;
 		i++;
 	}
 }
