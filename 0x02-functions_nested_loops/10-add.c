3f7010325e694eb693506717015a2f675c2fa956
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-22 16:47:25 -0800

    sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/*function declaration*/
+int add(int, int);
+
+
+/**
+ * add - return sum of two numbers
+ * Return: return int
+ * @i: this first variable
+ * @j: this second variable
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int print_sign(int n);
 int _abs(int n);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 int print_last_digit(int n);
 int print_Putchar(void);
 void print_alphabet(void);
