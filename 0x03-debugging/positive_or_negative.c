30243d7bdce62b0f60697d22ba718e6a385e405e
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-24 00:35:02 +0100

    function pos neg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* positive_or_negative - tests function that prints if integer is positive or negative
+* Return: void
+*/
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
