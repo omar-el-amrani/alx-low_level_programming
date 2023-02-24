a3b0145d3fe0bf60517a19c8b8b489e58340bf8d
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-24 02:41:11 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * _isupper - checks for positive or negative numbers
+  *
+  * @c: the char to be checked
+  *
+  * Return: void
+  */
+
+int _isupper(int c)
+{
+	if (65 <= c && 90 >= c)
+		return (1);
+	if(97 <= c && 122 >= c)
+		return (0);
+}
