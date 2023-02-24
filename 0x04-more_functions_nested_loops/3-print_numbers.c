886e26b5e2c1a4842bcc3e39cbe1a25886d5ac03
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-24 03:46:02 -0800

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * print_numbers - print number from 0 to 9
+  *
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	int i;	
+	
+	for (i = 0;i < 10;i++)
+		printf("%d",i);
+}
