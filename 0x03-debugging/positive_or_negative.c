5704fcdce73f83f58a224a610f40f662ce9e5de7
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-23 16:36:07 -0800

    first

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* positive_or_negative - tests function that prints if integer is positive or negative
+* positive_or_negative - tests function that prints if integer is pos or neg
+* @n: this for variable n
 * Return: void
 */
 
