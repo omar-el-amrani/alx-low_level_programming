0ceba17a1d875054b01701de7113b3701ea6d3b8
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-24 17:16:30 -0800

    square func

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
-  * print_diagonal - print diagonal
-  * @n: this for n variable
+  * print_square - print diagonal
+  * @size: this for size variable
   * Return: void
   */
-void print_square(int size);
+void print_square(int size)
 {
 	int i;
 	int j;
 
-	if (size >= 0)
-		("\n");
-	for (i = 0; i < n; i++)
+	if (size <= 0)
+		putchar('\n');
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < i; j++)
-			printf(" ");
-		printf("\\\n");
+		for (j = 0; j < size; j++)
+			putchar('#');
+		putchar('\n');
 	}
 }
