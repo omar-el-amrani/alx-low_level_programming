c5d66d6148d58cac3ae403473500d048ec6dc606
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-27 13:21:21 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * _strlen - function that  returns the length of string
+  *
+  * @s: string varible
+  * Return: return lenght of string s
+  */
+
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
