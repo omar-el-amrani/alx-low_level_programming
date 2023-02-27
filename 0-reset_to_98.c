9b7b62c5a6ec63050ca28ce9538e0065edec63f6
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-27 12:57:44 -0800

    reset to 98

---
+++ 0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * reset_to_98 - update varible given as arg with 98
+  *
+  * @*n: the first pointer of int number
+  *
+  * Return: return void
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
