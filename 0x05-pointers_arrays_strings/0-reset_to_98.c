334e5df3d66cd1d544c1ba607d63c5b98f814a44
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-27 13:01:52 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
   * reset_to_98 - update varible given as arg with 98
   *
-  * @*n: the first pointer of int number
+  * @n: the first pointer of int number
   *
   * Return: return void
   */
