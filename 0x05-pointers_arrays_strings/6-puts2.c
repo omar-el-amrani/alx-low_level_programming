7eba9f19f026d9735f277103175d287da58d65ff
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 03:09:43 -0800

    starting with first char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * puts2 - print str
+  *
+  * @s: the first car
+  *
+  * Return: return void
+  */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		if (count % 2 = 0)
+		printf("%c", *s);
+		count++;
+		s++;
+	}
+	printf("\n");
+}
