0b155b2edca4fbc67bdfcceea4e39d3148971f6f
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 03:38:03 -0800

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,16 +12,16 @@ void puts_half(char *str)
 	int len,half;
 
 	len = 0;
-	while (*(s + len) != '\0')
+	while (*(str + len) != '\0')
 	{
 		len++;
 	}
 	half = len / 2;
 	len = 0;
-	while (*(s + len) != '\0')
+	while (*(str + len) != '\0')
 	{
 		if ( len >= half)
-			printf("%c", *(s + len));
+			printf("%c", *(str + len));
 		len++;
 	}
 }
