85da2e5b7f57313d4cd328ece094e8b104861462
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 13:52:32 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * *_strcpy - print str
+  *
+  * @dest: the first var
+  * @src: the second var
+  * Return: return char
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+i=0;
+if(dest == NULL)
+	{
+    return NULL;
+    }
+while(*(src+i) !='\0')
+    {
+    	dest[i]=src[i];
+        i++;
+    }
+    return dest;
+}
