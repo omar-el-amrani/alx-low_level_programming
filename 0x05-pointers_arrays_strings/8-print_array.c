27bd60758eb30edfb66529cca7810e1affb561df
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 12:25:13 -0800

    print n element of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+  * print_array - print str
+  *
+  * @a: the first var
+  * @n: the second var
+  * Return: return void
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%c,", a[i]);
+		}
+		else if (i == (n - 1))
+		{
+			printf(" %c", a[i]);
+		}
+		else
+		{
+			printf(" %c,", a[i]);
+		}
+	}
+	printf("\n");
+}
