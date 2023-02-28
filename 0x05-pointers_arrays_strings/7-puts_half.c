828cf407722d7d25cc512d82ef8df978d22071ae
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 03:35:15 -0800

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * puts_half - function that print half string
+  *
+  * @str: string varible
+  * Return: return void
+  */
+
+void puts_half(char *str)
+{
+	int len,half;
+
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	half = len / 2;
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		if ( len >= half)
+			printf("%c", *(s + len));
+		len++;
+	}
+}
