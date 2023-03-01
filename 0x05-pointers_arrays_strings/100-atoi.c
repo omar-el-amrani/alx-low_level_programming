5b9896b72b330550a12c13ab64c323164eb725db
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 18:07:26 -0800

    convert string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * _atoi - convert string to int
+  *
+  * @s: the second var
+  * Return: return int
+  */
+int _atoi(char *s)
+{
+	int i, j;
+	char sumInt[100];
+
+	j = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if ((s[i] == '+' && s[i + 1] >= 48 && s[i + 1] <= 57) || (s[i] == '-' && s[i + 1] >= 48 && s[i + 1] <= 57) || (s[i] >= 48 && s[i] <= 57))
+		{
+			sumInt[j] = s[i];
+			j++;
+		}
+		if (s[i] >= 48 && s[i] <= 57 && s[i+1] == ' ')
+		{
+			return (atoi(sumInt));
+		}
+		i++;
+	}
+	return (atoi(sumInt));
+}
