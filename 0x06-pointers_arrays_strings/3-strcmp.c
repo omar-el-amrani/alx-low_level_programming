65e5d3860b90fc9485f095e04952651dd033c321
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-01 14:21:40 -0800

    compare two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,12 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	while (*(s1 + i) != '\0')
+	while (*(s1 + i) != '\0' || *(s2 + j) != '\0')
 	{
+		if (*(s1 + i) != *(s2 + j))
+			return (*(s1 + i) - *(s2 + j));
 		i++;
-	}
-	while (*(s2 + j) != '\0')
-	{
 		j++;
 	}
-	if (i == j)
-		return (0);
-	if (i > j)
-		return (15);
-	return (-15);
+	return (0);
 }
