42389c0bb9cefa24f77a5530b3e879381a4a25eb
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-02-28 17:26:29 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,25 +10,22 @@
 void rev_string(char *s)
 {
 	int count,i,half;
-        char str;
-        
-        i=0;
-        count=0;
-        while (*(s+count) != '\0')
-        {
-        	count++;
-        	
-        }
+	char str;
+
+	i=0;
+	count=0;
+	while (*(s+count) != '\0')
+	{
+		count++;
+	}
 	half = count / 2;
 	count--;
-		while (i < half)
+	while (i < half)
 	{
-		printf("in while this half: %d  and this count: %d and this is I: %d and this two char1: %c ,char2: %c",half,count,i,s[i],s[count]);
 		str=s[i];
 		s[i] = s[count];
-            s[count] = str;	
-            count--;
-            i++; 
-           
-        }
+		s[count] = str;
+		count--;
+		i++;
+	}
 }
