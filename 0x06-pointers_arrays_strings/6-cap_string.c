eedb1c05b03faa418ba808bcf3203a300a6725a2
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-02 06:27:43 -0800

    make uppercase after separators

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * *cap_string - compare two strings length
+  * @str: the first var
+  * Return: return char
+  */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == 44 || *(str + i) == 59 || *(str + i) == 33 || *(str + i) == 46 || *(str + i) == 63 || *(str + i) == 34 || *(str + i) == 40 || *(str + i) == 41 || *(str + i) == 123 || *(str + i) == 125)
+			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
+				*(str + i + 1) = *(str + i + 1) - 32;
+		i++;
+	}
+	return (str);
+}
