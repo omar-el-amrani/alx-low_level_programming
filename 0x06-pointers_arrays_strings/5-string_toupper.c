6b5a7eaed0514c9cc67abeb4e4b432a56bf099c1
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-01 19:22:45 -0800

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * *string_toupper - compare two strings length
+  * @str: the first var
+  * Return: return char
+  */
+char *string_toupper(char *str);
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 97 && *(str + i) <= 122)
+			*(str + i) = *(str + i) - 32;
+		i++;
+	}
+}
