01824063cfdbe67d7f898a4bfb49acd10010d44f
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-02 10:29:06 -0800

    capitalizes all words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,32 +10,27 @@ char *cap_string(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		if (*(str + i + 1) != '\0')
+		if (*(str + i + 1) != '\0' && *(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 		{
 			if (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == 44)
 			{
-				if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
-					*(str + i + 1) = *(str + i + 1) - 32;
+				*(str + i + 1) = *(str + i + 1) - 32;
 			}
 			else if (*(str + i) == 59 || *(str + i) == 33 || *(str + i) == 40)
 			{
-				if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
-					*(str + i + 1) = *(str + i + 1) - 32;
+				*(str + i + 1) = *(str + i + 1) - 32;
 			}
 			else if (*(str + i) == 32)
 			{
-				if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
-					*(str + i + 1) = *(str + i + 1) - 32;
+				*(str + i + 1) = *(str + i + 1) - 32;
 			}
 			else if (*(str + i) == 46 || *(str + i) == 63 || *(str + i) == 34)
 			{
-				if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
-					*(str + i + 1) = *(str + i + 1) - 32;
+				*(str + i + 1) = *(str + i + 1) - 32;
 			}
 			else if (*(str + i) == 41 || *(str + i) == 123 || *(str + i) == 125)
 			{
-				if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
-					*(str + i + 1) = *(str + i + 1) - 32;
+				*(str + i + 1) = *(str + i + 1) - 32;
 			}
 		}
 		i++;
