26cc1aaea92e129f9fde67002bb7d6d5e954b84c
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-02 09:11:14 -0800

    capitalizes all words of string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
-  * *cap_string - compare two strings length
+  * *cap_string - capitalizes all words
   * @str: the first var
   * Return: return char
   */
@@ -12,20 +11,30 @@ char *cap_string(char *str)
 	while (*(str + i) != '\0')
 	{
 		if (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == 44)
+		{
 			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 				*(str + i + 1) = *(str + i + 1) - 32;
-		if (*(str + i) == 59 || *(str + i) == 33 || *(str + i) == 40)
+		}
+		else if (*(str + i) == 59 || *(str + i) == 33 || *(str + i) == 40)
+		{
 			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 				*(str + i + 1) = *(str + i + 1) - 32;
-		if (*(str + i) == 32)
+		}
+		else if (*(str + i) == 32)
+		{
 			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 				*(str + i + 1) = *(str + i + 1) - 32;
-		if (*(str + i) == 46 || *(str + i) == 63 || *(str + i) == 34)
+		}
+		else if (*(str + i) == 46 || *(str + i) == 63 || *(str + i) == 34)
+		{
 			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 				*(str + i + 1) = *(str + i + 1) - 32;
-		if (*(str + i) == 41 || *(str + i) == 123 || *(str + i) == 125)
+		}
+		else if (*(str + i) == 41 || *(str + i) == 123 || *(str + i) == 125)
+		{
 			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 				*(str + i + 1) = *(str + i + 1) - 32;
+		}
 		i++;
 	}
 	return (str);
