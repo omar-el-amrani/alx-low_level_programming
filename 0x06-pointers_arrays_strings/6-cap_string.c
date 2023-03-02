e63cb8f92be23f9a42b5407ff8d02df9d3fceff6
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-02 06:37:15 -0800

    make uppercase after separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		if (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == 44 || *(str + i) == 59 || *(str + i) == 33 || *(str + i) == 46 || *(str + i) == 63 || *(str + i) == 34 || *(str + i) == 40 || *(str + i) == 41 || *(str + i) == 123 || *(str + i) == 125)
+		if (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == 44 || *(str + i) == 59 || *(str + i) == 33 || *(str + i) == 46 || *(str + i) == 63 || *(str + i) == 34 || *(str + i) == 40 || *(str + i) == 41 || *(str + i) == 123 || *(str + i) == 125 || *(str + i) == 32)
 			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 				*(str + i + 1) = *(str + i + 1) - 32;
 		i++;
