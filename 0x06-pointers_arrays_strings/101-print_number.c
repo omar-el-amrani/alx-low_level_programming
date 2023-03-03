c9532fb7444d23ac253d65c9519cd48516749c46
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-02 16:56:32 -0800

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+  * print_number - encode a string using rot13
+  * @n: the first var
+  * Return: return void
+  */
+void print_number(int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int op = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		op *= -1;
+	}
+	if (op / 10 > 0)
+	{
+		i = op % 10;
+		op /= 10;
+		if (op / 10 > 0)
+		{
+			j = op % 10;
+			op /= 10;
+			if (op / 10 > 0)
+			{
+				k = op % 10;
+				op /= 10;
+				if (op / 10 > 0)
+				{
+					l = op % 10;
+				}
+			}
+		}
+	}
+	if (l > 0)
+	{
+		_putchar('0' + l);
+		_putchar('0' + k);
+		_putchar('0' + j);
+		_putchar('0' + i);
+	}
+	else if (k > 0)
+	{
+		_putchar('0' + k);
+		_putchar('0' + j);
+		_putchar('0' + i);
+	}
+	else if (j > 0)
+	{
+		_putchar('0' + j);
+		_putchar('0' + i);
+	}
+	else if (i > 0)
+		_putchar('0' + i);
+
+}
