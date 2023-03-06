2c7fb894af9a2a6efacbaa9a139f035b2f021a9b
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-06 07:33:12 -0800

    copy n  bytes from array to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * *_memcpy - copy n bytes from array to another
+  * @dest: the first var
+  * @src: the second var
+  * @n: the third var
+  * Return: return char
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (s);
+}
