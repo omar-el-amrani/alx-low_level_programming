56dd4b76963eb3a8569edbdaeb0d75f7ab99a093
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-07 07:23:57 -0800

    test ex 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * print_chessboard - print array string
+  * @a: the first var
+  * Return: return void
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (*(*(a + j) + i) != '\0')
+		{
+			_putchar(*(*(a + i) + j));
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
