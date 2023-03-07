20a9dd45e2c5875356fe3c5015fe510e8e5480fb
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-07 06:00:52 -0800

    test ex 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+  * *_strstr - find string in another string
+  * @haystack: the first var
+  * @needle: the second var
+  * Return: return char
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k, t;
+
+	j = 0;
+	i = 0;
+	k = 0;
+	t = 0;
+	while (*(haystack + i) !=  '\0')
+	{
+		k = i;
+		j = 0;
+		t = 0;
+		while (*(needle + j) != '\0')
+		{
+			if (*(haystack + k) == *(needle + j))
+				t++;
+			j++;
+			k++;
+		}
+		if (t == j)
+			return (needle);
+		i++;
+	}
+	return (NULL);
+}
+
