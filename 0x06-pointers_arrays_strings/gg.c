cea94a225f9e27393dd3ba399baa06d6f4217621
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-07 08:17:58 -0800

    7 pointer

---
+++ 0x06-pointers_arrays_strings/gg.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_number - encode a string using rot13
+  * @n: the first var
+  * Return: return void
+  */
+void print_number(int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int op = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		op *= -1;
+	}
+	if (op / 10 > 0)
+	{
+		i = op % 10;
+		if (op / 10 > 0)
+		{
+			op /= 10;
+			j = op % 10;
+			if (op / 10 > 0)
+			{
+				op /= 10;
+				k = op % 10;
+				if (op / 10 > 0)
+				{
+					op /= 10;
+					l = op % 10;
+				}
+			}
+		}
+	}
+	if (l > 0)
+		_putchar('0' + l);
+	if (k > 0)
+		_putchar('0' + k);
+	if (j > 0)
+		_putchar('0' + j);
+	if (i > 0)
+		_putchar('0' + i);
+}
