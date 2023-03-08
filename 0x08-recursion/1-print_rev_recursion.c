a7de79440c352b83cec21503ba5755a96440c676
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-08 09:14:50 -0800

    print string reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * _print_rev_recursion - print string
+  * @s: the first var
+  * Return: return void
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 0) == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*(s + 0));
+}
