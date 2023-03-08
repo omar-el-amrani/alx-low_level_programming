258db861bf35deac75bb116fdc7c8c508ec0f52e
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-08 04:17:22 -0800

    function recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * _puts_recursion- print string
+  * @s: the first var
+  * Return: return char
+  */
+void _puts_recursion(char *s)
+{
+	if (*(s + 0) != '\0')
+		return;
+	_putchar(*(s + 0));
+	_puts_recursion(*(s + 1));
+}
