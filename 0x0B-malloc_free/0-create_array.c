b7e941c57119a1591c4ce79c244fe9b92b2b2ca4
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-14 12:32:10 -0700

    malloc tp

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * *create_array - test if number has sqrt
+  * @size: the first var
+  * @c: the second var
+  * Return: return array
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	i = 0;
+	*s = malloc(size * sizeof(char));
+	while (i < size)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
