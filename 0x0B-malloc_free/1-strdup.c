efae2964a2ceab12f02149ea31a4c22236177b71
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-14 13:13:00 -0700

    copy str to str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * *_strdup - copy str to str
+  * @str: the first var
+  * Return: return string
+  */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, size;
+
+	size = sizeof(str);
+	i = 0;
+	s = malloc(size * sizeof(char));
+	if (s == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
