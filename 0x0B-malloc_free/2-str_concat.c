590a2dd38e9fca4de05ffb5f2ee1d4a57d3f0987
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-14 17:38:32 -0700

    concat two str

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * *str_concat - concat str to str
+  * @s1: the first var
+  * @s2: the second var
+  * Return: return string
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, size1, size2;
+
+	size1 = 0;
+	size2 = 0;
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 != NULL)
+		size1 = strlen(s1) + 1;
+	if (s2 != NULL)
+		size2 = strlen(s2) + 1;
+	i = 0;
+	s = malloc((size1 + size2) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (i < (size1 + size2))
+	{
+		if (i < size1)
+			s[i] = s1[i];
+		else if ((i - size1) < size2)
+			s[i] = s2[i - size1];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
