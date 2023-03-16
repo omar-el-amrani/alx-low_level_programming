fbdce4ce65e671f04d3001dfcdc995150e5fa7a2
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-15 17:31:14 -0700

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * **alloc_grid - make array in array
+  * @width: the first var
+  * @height: the second var
+  * Return: return array
+  */
+int **alloc_grid(int width, int height)
+{
+	int **ar;
+	int i, j;
+
+	i = 0;
+	j = 0;
+	ar = malloc(height * sizeof(int));
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	while (i < height)
+	{
+		ar[i] = malloc(width * sizeof(int));
+		if (ar[i] == NULL)
+			return (NULL);
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		while (j < width)
+		{
+			ar[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (ar);
+}
