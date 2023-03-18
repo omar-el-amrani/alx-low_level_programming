b217b9e1564bb338bb3e2c9acaba55b5c9781804
omar-el-amrani <omar.amrani.idrissi@gmail.com>
2023-03-17 19:57:48 -0700

    last test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,8 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = strlen(s1) - 1;
-	len2 = strlen(s2) - 1;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 	if (sign >= len2)
 	{
 		sign = len2;
